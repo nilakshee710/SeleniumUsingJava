f18b96a8a3738d4142dc268e61b17c9230d5be85
nilakshee710 <nilakshee710@gmail.com>
2022-10-29 15:52:05 -0400

    if statement code

---
+++ src/code3.java
@@ -0,0 +1,18 @@
+
+public class code3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean result1 = false;
+		boolean result2 = false;
+		if (result1) {
+			System.out.println("result1 is True");
+		} else if (result2) {
+			System.out.println("result2 is True");
+		} else {
+			System.out.println("both result1 and result2 are False");
+		}
+
+	}
+
+}
