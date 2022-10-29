3034cdc32a102f52ef22d7a5e4b4da2a204bee51
nilakshee710 <nilakshee710@gmail.com>
2022-10-29 16:06:27 -0400

    new method to add double

---
+++ src/methods.java
@@ -0,0 +1,27 @@
+
+public class methods {
+	
+
+	public static int calculateSum(int num1, int num2) {
+		// TODO Auto-generated method stub
+		int sum = num1+num2;
+		return sum;
+	}
+
+	public static double calculateDouble(double num1, double num2) {
+		// TODO Auto-generated method stub
+		double sum = num1+num2;
+		return sum;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(calculateSum(6,6));
+		System.out.println(calculateDouble(6.2,6.8));
+		
+		
+		
+		
+	}
+
+}
